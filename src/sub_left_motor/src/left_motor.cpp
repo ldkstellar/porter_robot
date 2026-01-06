5bcb94aaab2a974121fe7deb40fba0276ef81961
leedongkyu <leedongkyu0225@gmail.com>
2026-01-06 04:08:29 +0000

    feat: add motor subscriber

---
+++ src/sub_left_motor/src/left_motor.cpp
@@ -0,0 +1,33 @@
+#include <memory>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+using std::placeholders::_1;
+
+class LmotorSubscriber : public rclcpp::Node
+{
+  public:
+    LmotorSubscriber()
+    : Node("left_motor")
+    {
+      subscription_ = this->create_subscription<std_msgs::msg::String>(
+      "left", 10, std::bind(&LmotorSubscriber::topic_callback, this, _1));
+    }
+
+  private:
+    void topic_callback(const std_msgs::msg::String & msg) const
+    {
+        // 모터제어로직 타이머 콜백은 퍼블리시 쪽에서 짧게 줘야 한다.
+
+      RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg.data.c_str());
+    }
+    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<LmotorSubscriber>());
+  rclcpp::shutdown();
+  return 0;
+}
